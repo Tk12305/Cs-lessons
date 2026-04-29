a84b933f8955fe58e2e83841fe7b52697daf978e
Toby king <tbgglking@gmail.com>
2026-04-29 16:05:39 +0000

    A ocr paper2 question from 2022 paper

---
+++ Paper2 question.py
@@ -0,0 +1,10 @@
+num = 0
+num_quantity = int(input("Enter Number quantity"))
+
+for i in range (0, num_quantity):
+    num += int(input("Enter a Number"))
+print(num)
+
+avg_num = (num / num_quantity)
+
+print(avg_num)
