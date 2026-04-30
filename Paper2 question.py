68d4dfce2470db9dc778b63f61302e64f0a959ed
Toby king <tbgglking@gmail.com>
2026-04-30 13:32:44 +0000

    Code update

--- Paper2 question.py
+++ Paper2 question.py
@@ -1,8 +1,8 @@
 num = 0
-num_quantity = int(input("Enter Number quantity"))
+num_quantity = int(input("Enter Number quantity: "))
 
 for i in range (0, num_quantity):
-    num += int(input("Enter a Number"))
+    num += int(input("Enter a Number: "))
 print(num)
 
 avg_num = (num / num_quantity)
